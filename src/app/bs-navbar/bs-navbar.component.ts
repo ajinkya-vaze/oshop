fb143b8769bcb8c1a28f7c3c415ae1ec40cf782b
Ajinkya Vaze <ajinkyav@gmail.com>
2018-06-03 13:53:18 +0530

    Forced to use app prefix by linting

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -3,7 +3,7 @@ import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bs-navbar',
+  selector: 'app-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
